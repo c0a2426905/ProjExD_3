ae7a05e8816bf1e44b9d00ae070f61b353d23dd9
c0a24269 <c0a2426905@edu.teu.ac.jp>
2025-12-02 15:57:19 +0900

    追加機能1:スコア表示 完了

--- fight_kokaton.py
+++ fight_kokaton.py
@@ -141,6 +141,20 @@ class Bomb:
         screen.blit(self.img, self.rct)
 
 
+class Score:
+    def __init__(self):
+        self.fonto = pg.font.SysFont("hgp創英角ﾎﾟｯﾌﾟ体", 30)
+        self.color = ((0, 0, 255))
+        self.score = 0
+        self.img = self.fonto.render(f"スコア：{self.score}", 0, self.color)
+        self.img_rect = self.img.get_rect()
+        self.img_rect.center= 100, 600
+
+    def update(self, screen: pg.Surface):
+        self.img = self.fonto.render(f"スコア：{self.score}", 0, self.color)
+        screen.blit(self.img, self.img_rect)
+        
+
 def main():
     pg.display.set_caption("たたかえ！こうかとん")
     screen = pg.display.set_mode((WIDTH, HEIGHT))    
@@ -153,6 +167,7 @@ def main():
     #     bombs.append(bomb)
     bombs = [Bomb((255, 0, 0), 10) for _ in range(NUM_OF_BOMS)]
     beam = None  # ゲーム初期化時にはビームは存在しない
+    score = Score()
     clock = pg.time.Clock()
     tmr = 0
     while True:
@@ -182,6 +197,7 @@ def main():
                     beam = None
                     bombs[b] = None
                     bird.change_img(6, screen)
+                    score.score += 1
                     pg.display.update()
         bombs = [bomb for bomb in bombs if bomb is not None]
 
@@ -191,6 +207,7 @@ def main():
             beam.update(screen)
         for bomb in bombs:  # 爆弾が存在していたら   
             bomb.update(screen)
+        score.update(screen)
         pg.display.update()
         tmr += 1
         clock.tick(50)
